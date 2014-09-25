0292acf97848d9d605491d4c3b087971c2facc43
paramat <mat.gregory@virginmedia.com>
2014-09-25 08:55:49 +0100

    64x64 cloud. Spawn over grass in high humidity away from deserts. Fix cloud eating floatlands

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-rain 0.1.2 by paramat
+rain 0.1.3 by paramat
 For latest stable Minetest back to 0.4.8
 Depends default
 Licenses: code WTFPL, textures CC BY-SA
--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
--- rain 0.1.2 by paramat
+-- rain 0.1.3 by paramat
 -- For latest stable Minetest and back to 0.4.8
 -- Depends default
 -- License: code WTFPL, textures CC BY-SA
 
--- Lower cloud. dissolve over desert
--- TODO
 -- 64x64 XZ raincloud at y = 64
--- spawns over northern coast near deep water: check for deep water and scan south for dirt
--- before cloud spawn check for desert sand -> return
+-- spawns over grass in more mgv6-humid areas, also away from deserts by biome noise
+-- fix cloud eating floatlands
+-- TODO
+-- move 16 nodes at a time less frequently
 
 -- Nodes
 
@@ -21,7 +21,6 @@ minetest.register_node("rain:cloud", {
 	pointable = false,
 	diggable = false,
 	buildable_to = true,
-	post_effect_color = {a=127, r=162, g=166, b=171},
 })
 
 minetest.register_node("rain:rain", {
@@ -64,26 +63,32 @@ minetest.register_node("rain:rainabm", {
 
 -- ABM
 
--- Spawn raincloud above northern coast near deep water
+-- Spawn raincloud
 
 minetest.register_abm({
-	nodenames = {"default:water_source"},
-	interval = 3,
-	chance = 32768,
+	nodenames = {"default:dirt_with_grass"},
+	interval = 61,
+	chance = 4096,
 	action = function(pos, node)
 		local x = pos.x
 		local y = pos.y
 		local z = pos.z
+
+		local desnoise = minetest.get_perlin(9130, 3, 0.5, 250) -- check biome and humidity
+		local humnoise = minetest.get_perlin(72384, 4, 0.66, 500)
+		if not (desnoise:get2d({x=x+150,y=z+50}) < -0.4
+		and humnoise:get2d({x=x+250,y=z+250}) > 0.4) then
+			return
+		end
+
 		local c_air = minetest.get_content_id("air")
-		local c_water = minetest.get_content_id("default:water_source")
-		local c_dirt = minetest.get_content_id("default:dirt")
 		local c_rain = minetest.get_content_id("rain:rain")
 		local c_rainabm = minetest.get_content_id("rain:rainabm")
 		local c_cloud = minetest.get_content_id("rain:cloud")
 
 		local vm = minetest.get_voxel_manip() -- large flat volume for cloud check
-		local pos1 = {x=x-128, y=64, z=z-128}
-		local pos2 = {x=x+159, y=64, z=z+159}
+		local pos1 = {x=x-160, y=64, z=z-144}
+		local pos2 = {x=x+159, y=64, z=z+175}
 		local emin, emax = vm:read_from_map(pos1, pos2)
 		local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
 		local data = vm:get_data()
@@ -95,20 +100,22 @@ minetest.register_abm({
 		end
 
 		local vm = minetest.get_voxel_manip() -- spawn cloud and rain columns
-		local pos1 = {x=x, y=1, z=z}
-		local pos2 = {x=x+31, y=111, z=z+31}
+		local pos1 = {x=x-32, y=1, z=z-16}
+		local pos2 = {x=x+31, y=95, z=z+47}
 		local emin, emax = vm:read_from_map(pos1, pos2)
 		local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
 		local data = vm:get_data()
 		local vvii = emax.x - emin.x + 1 -- vertical vi interval
 		-- local nvii = (emax.y - emin.y + 1) * vvii
 
-		for y = 64, 111 do
-		for k = 0, 31 do
-			local vi = area:index(x, y, z + k)
-			for i = 0, 31 do
-				data[vi] = c_cloud
-				if y == 64 and k == 0 and i == 15 then -- rain abm column
+		for y = 64, 95 do
+		for k = -16, 47 do
+			local vi = area:index(x-32, y, z + k)
+			for i = -32, 31 do
+				if data[vi] == c_air then
+					data[vi] = c_cloud
+				end
+				if y == 64 and k == 0 and i == 0 then -- rain abm column
 					local vir = vi - vvii
 					for fall = 1, 63 do
 						if data[vir] == c_air then
@@ -118,7 +125,8 @@ minetest.register_abm({
 						end
 						vir = vir - vvii
 					end
-				elseif y == 64 and math.random() < 0.25 then
+				elseif y == 64 and i >= -16 and i <= 15 -- rain columns
+				and k >= 0 and k <= 31 and math.random() < 0.25 then
 					local vir = vi - vvii
 					for fall = 1, 63 do
 						if data[vir] == c_air then
@@ -145,27 +153,26 @@ minetest.register_abm({
 minetest.register_abm({
 	nodenames = {"rain:rainabm"},
 	interval = 7,
-	chance = 128,
+	chance = 64,
 	action = function(pos, node)
 		local x = pos.x
 		local z = pos.z
 		local c_air = minetest.get_content_id("air")
-		local c_water = minetest.get_content_id("default:water_source")
 		local c_desand = minetest.get_content_id("default:desert_sand")
 		local c_rain = minetest.get_content_id("rain:rain")
 		local c_rainabm = minetest.get_content_id("rain:rainabm")
 		local c_cloud = minetest.get_content_id("rain:cloud")
 
 		local vm = minetest.get_voxel_manip()
-		local pos1 = {x=x-15, y=1, z=z-1}
-		local pos2 = {x=x+16, y=111, z=z+31}
+		local pos1 = {x=x-32, y=1, z=z-16}
+		local pos2 = {x=x+31, y=95, z=z+47}
 		local emin, emax = vm:read_from_map(pos1, pos2)
 		local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
 		local data = vm:get_data()
 		local vvii = emax.x - emin.x + 1 -- vertical vi interval
 
 		local dissolve = false -- check ground for desert sand
-		for vi in area:iterp(pos1, {x=x+16, y=47, z=z+31}) do
+		for vi in area:iterp(pos1, {x=x+31, y=47, z=z+47}) do
 			if data[vi] == c_desand then
 				dissolve = true
 				break
@@ -186,14 +193,16 @@ minetest.register_abm({
 			return
 		end
 
-		for y = 63, 111 do -- spawn and erase cloud and rain columns
-		for k = -1, 31 do
-			local vi = area:index(x-15, y, z+k)
-			for i = -15, 16 do
-				if k == -1 then
-					if y >= 64 then -- new cloud
+		for y = 63, 95 do -- spawn and erase cloud and rain columns
+		for k = -16, 47 do
+			local vi = area:index(x-32, y, z+k)
+			for i = -32, 31 do
+				if k == -16 then
+					if y >= 64 and data[vi] == c_air then -- new cloud
 						data[vi] = c_cloud
-					elseif y == 63 and i == 0 then -- new rain abm column
+					end
+				elseif k == -1 and y == 63 then
+					if i == 0 then -- new rain abm column
 						local vir = vi
 						for fall = 1, 63 do
 							if data[vir] == c_air then
@@ -203,7 +212,8 @@ minetest.register_abm({
 							end
 							vir = vir - vvii
 						end
-					elseif y == 63 and math.random() < 0.25 then -- new rain
+					elseif i >= -16 and i <= 15
+					and math.random() < 0.25 then -- new rain
 						local vir = vi
 						for fall = 1, 63 do
 							if data[vir] == c_air then
@@ -214,7 +224,7 @@ minetest.register_abm({
 							vir = vir - vvii
 						end
 					end
-				elseif k == 0 and y == 63 and i == 0 then -- erase previous rain abm column
+				elseif k == 0 and y == 63 and i == 0 then -- erase previous rainabm column
 					local vir = vi
 					for fall = 1, 63 do
 						if data[vir] == c_rainabm then
@@ -224,11 +234,10 @@ minetest.register_abm({
 						end
 						vir = vir - vvii
 					end
-				elseif k == 31 and y >= 63 then -- erase previous cloud and rain
+				elseif k == 31 and y == 63 
+				and i >= -16 and i <= 15 then -- erase previous rain
 					local nodid = data[vi]
-					if nodid == c_cloud then
-						data[vi] = c_air
-					elseif nodid == c_rain then
+					if nodid == c_rain then
 						local vir = vi
 						for fall = 1, 63 do
 							if data[vir] == c_rain then
@@ -238,6 +247,11 @@ minetest.register_abm({
 							end
 							vir = vir - vvii
 						end
+					end
+				elseif k == 47 and y >= 64 then -- erase previous cloud
+					local nodid = data[vi]
+					if nodid == c_cloud then
+						data[vi] = c_air
 					end
 				end
 				vi = vi + 1
